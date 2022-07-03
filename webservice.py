16095169cecaa91559b71b96b1681744bcdd70bf
misterplus <whiteplayz45@gmail.com>
2022-07-03 11:31:49 +0800

    res

--- webservice.py
+++ webservice.py
@@ -13,7 +13,9 @@ class FaceWebService(WebService):
     def get_prediction(self, request):
         if not request.json:
             abort(400)
-        if request.json["feed"]["threshold"] < 0 or request.json["feed"]["threshold"] > 1:
+        if "threshold" in request.json["feed"] and (request.json["feed"]["threshold"] < 0 or request.json["feed"]["threshold"] > 1):
+            abort(400)
+        if "size" in request.json["feed"] and request.json["feed"]["size"] < 1:
             abort(400)
         try:
             feed, fetch, is_batch = self.preprocess(request.json["feed"],
@@ -42,9 +44,9 @@ class FaceWebService(WebService):
     def postprocess(self, feed={}, fetch=[], fetch_map=None):
         for key in fetch_map:
             fetch_map[key] = fetch_map[key].tolist()
-        threshold = feed['threshold'] if feed['threshold'] else 0.9
+        threshold = feed['threshold'] if 'threshold' in feed else 0.9
         fetch_map['faces'] = [arr[1:] for arr in fetch_map['multiclass_nms3_0.tmp_0'] if arr[1] > threshold]
-        size = feed['size'] if feed['size'] else len(fetch_map['faces'])
+        size = feed['size'] if 'size' in feed else len(fetch_map['faces'])
         fetch_map['faces'] = fetch_map['faces'][0:size]
         fetch_map['size'] = len(fetch_map['faces'])
         del fetch_map['multiclass_nms3_0.tmp_0']
